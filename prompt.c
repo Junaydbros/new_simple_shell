5a17e7e3678159fa8a88934213d40b9366278af2
Isaackaayyy <kammaokoh@Kays-Computer.localdomain>
2023-07-26 02:54:33 +0100

    Simple shell

---
+++ prompt.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * prompt1 - Displaying the prompt for the user
+ */
+
+void prompt1(void)
+{
+	fprintf(stderr, "Shell:$ ");
+}
+
+
+/**
+ * prompt2 - Displayint the prompt for the user
+ */
+
+void prompt2(void)
+{
+	fprintf(stderr, "Shell:$ ");
+}
